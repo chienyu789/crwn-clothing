65933485e213f32548a4036aeb855074d339d86b
chienyu <alice7089@gmail.com>
2020-01-30 17:11:01 +0800

    route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Switch , Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = () =>(
+  <div>
+    <h1>123</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+      <Route eaxct path='/' component={HomePage}/>
+      <Route path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
